357e575e31512e737664dbeeba7224b7ffc75cf0
Nerzel <bouhmaneam@gmail.com>
2021-02-22 19:02:12 +0100

    Spider AI Import

---
+++ Source/Mech_Combat/SpiderBomb.h
@@ -0,0 +1,17 @@
+// Copyright Nerzel. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "DummyMaster.h"
+#include "SpiderBomb.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MECH_COMBAT_API ASpiderBomb : public ADummyMaster
+{
+	GENERATED_BODY()
+	
+};
